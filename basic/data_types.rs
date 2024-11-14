b64f0f7e73a79321275f2059df9ed57cedff92ad
Sabituddin Bigbang <sabituddin22@gmail.com>
2024-11-14 10:10:44 +0700

    Update data types

--- basic/data_types.rs
+++ basic/data_types.rs
@@ -60,22 +60,79 @@ fn this_is_integer() {
 fn this_is_floating_point() {
     let _float32bit: f32 = 23424.987654321;
     let _float64bit: f64 = 234242424.987654321;
-    println!(
-        "FLOATING POINT :\n- f32: {:?}\n- f64: {:?}\n",
-        _float32bit,
-        _float64bit,
-    );
+    println!("FLOATING POINT :\n- f32: {:?}\n- f64: {:?}\n", _float32bit, _float64bit);
 
     println!(
         "LIMIT FLOATING POINT :\n- f32:\t{:?} ... {:?}\n- f64:\t{:?} ... {:?}\n",
         f32::MIN,
         f32::MAX,
         f64::MIN,
-        f64::MAX,
+        f64::MAX
     );
+}
+
+fn this_is_char() {
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("CHAR : {} | {} | {} \n", c, z, heart_eyed_cat);
+}
+
+fn this_is_boolean() {
+    let t = true;
+    let f: bool = false;
+    println!("BOOLEAN :\nt: {}\nf: {}\n", t, f);
+}
+
+fn this_is_string() {
+    let s1: &str = "Sabituddin Bigbang";
+    println!("{}", s1);
+
+    let s2 = "Hello World!".to_string();
+    println!("{}", s2);
+
+    let s3 = String::from("Any body here?");
+    println!("{}", s3);
+
+    let mut s4 = s3; // pindahkan kepemilikan (ownership) nilai di s3 ke s4 | lalu free references (s3)
+    s4.push_str("...Gasskeun!!!");
+    println!("{}", s4);
+    // println!("{}\n", s3); // s3 was destroyed
+}
+
+fn this_is_tupple() {
+    let tup = (500, 43.4, 1, '😻');
+    println!("TUPPLE : {:?}\n", tup);
+    let (a, b, c, d) = tup;
+    println!("TUPPLE TO VARIABLE :\n- a : {}\n- b : {:?}\n- c : {}\n- d : {}\n", a, b, c, d);
+    println!(
+        "TUPPLE ELEMENT :\n- tup.0 : {}\n- tup.1 : {:?}\n- tup.2 : {}\n- tup.3 : {}\n",
+        tup.0,
+        tup.1,
+        tup.2,
+        tup.3
+    );
+}
+
+fn this_is_array() {
+    let data = [1, 2, 3, 4, 5]; // [values]
+    println!("ARRAY : {:?}", data);
+    for element in data.iter() {
+        println!("{}", element);
+    }
+
+    let mut a: [i8; 4] = [0; 4]; // [initial_value; length_of_data]
+    a[0] = 127;
+    a[3] = -128;
+    println!("A : {:?}", a);
 }
 
 fn main() {
     this_is_integer();
     this_is_floating_point();
+    this_is_char();
+    this_is_boolean();
+    this_is_string();
+    this_is_tupple();
+    this_is_array();
 }
